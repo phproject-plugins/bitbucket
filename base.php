c59014572774c4de10e14070233c27771dae4418
Alan Hardman <ahardman@thrivelife.com>
2014-11-06 09:31:33 -0700

    Initial functionality, untested

--- base.php
+++ base.php
@@ -15,7 +15,33 @@ class Base extends \Plugin {
 	 * @param array $params
 	 */
 	public function post($f3, $params) {
-		$post = json_decode(file_get_contents('php://input'), true);
+		if($f3->get("GET.token") == $f3->get("site.plugins.bitbucket.token")) {
+			$post = file_get_contents('php://input');
+			$json = json_decode($post);
+			foreach($json->commits as $commit) {
+				if(preg_match("/#[0-9]+/", $commit->message, $matches)) {
+					$id = intval(ltrim($matches[0], "#"));
+					$issue = new \Model\Issue;
+					$issue->load($id);
+					if($issue->id) {
+						$comment = new \Model\Comment;
+						$comment->issue_id = $issue->id;
+						if(preg_match("/<[^ ]+@[^ ]+>/", $commit->raw_author, $matches)) {
+							$user = new \Model\User;
+							$user->load(array("email = ?", trim($matches[0], "<>")));
+							if($user->id) {
+								$comment->user_id = $user->id;
+							}
+						}
+						$comment->text = "This issue was mentioned in a \"commit\":{$json->canon_url}{$json->repository->absolute_url}commits/{$commit->raw_node}:\n" . $commit->message;
+						$comment->created_date = $this->now();
+						$comment->save();
+					}
+				}
+			}
+		} else {
+			$f3->error(403);
+		}
 	}
 
 	/**
@@ -31,7 +57,7 @@ class Base extends \Plugin {
 		$f3 = \Base::instance();
 		$f3->set("success", "Bitbucket plugin installed. Hook URL: " . $f3->get("site.url") . "bitbucket-post?token=$token");
 		$f3->set("site.plugins.bitbucket.token", $token);
-		fwrite($fh, "[globals]\nsite.plugins.bitbucket.token=$token");
+		fwrite($fh, "[globals]\nsite.plugins.bitbucket.token=$token\n");
 		fclose($fh);
 	}
 
